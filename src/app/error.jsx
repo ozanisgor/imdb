4cb72593d51d0a669383e778b72f34b171eb0e45
ozan isgor <ozanisgor11@gmail.com>
2023-07-31 18:29:48 +0300

    ADD: handle the error

---
+++ src/app/error.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div className="text-center mt-10">
+      <h2>Something went wrong</h2>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
