1f62c9cf26ad6503f01e22b20f27e005d9108734
ozan isgor <ozanisgor11@gmail.com>
2023-07-31 10:12:56 +0300

    ADD: header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,24 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IMDB Clone",
-  description: "This is the IMDB clone website",
+  title: "IMDb Clone",
+  description: "This is the IMDb clone website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Navbar */}
+        <Header />
+        {/* Header */}
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
   );
 }
