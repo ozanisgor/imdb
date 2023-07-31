af236eb02beb4f84bfb565bb7c5f28d561e333d8
ozan isgor <ozanisgor11@gmail.com>
2023-07-31 16:01:49 +0300

    ADD: dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Navbar */}
-        <Header />
-        {/* Header */}
-        {/* SearchBox */}
-        {children}
+        <Providers>
+          {/* Navbar */}
+          <Header />
+          {/* Header */}
+          {/* SearchBox */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
